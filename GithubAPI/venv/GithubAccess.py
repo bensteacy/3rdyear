10c9aa760962eb9652847f0540726153ea1a3dfa
bensteacy <bensteacy@hotmail.com>
2018-11-08 17:29:14 +0000

    messing around pygithub api for user data representation

--- GithubAPI/venv/GithubAccess.py
+++ GithubAPI/venv/GithubAccess.py
@@ -1,12 +1,20 @@
 from github import Github
 g = Github("token")
-
+repo_count = 0
 user = g.get_user("phadej")
-print(user)
-print("list of repos:")
-for repo in user.get_repos():
-    print(repo.name)
-
+print("Name:",user.name)
+print("location:",user.location)
+user_repos = user.get_repos()
+print("No of Repos:",user_repos.totalCount)
+# for repo in user_repos:
+print("No of Contributions:",user.contributions)
+hask_api = user.get_repo("github")
+for content in hask_api.get_contents(""):
+    print(content)
+branch = hask_api.get_branch("master")
+head = branch.commit.sha
+gtree = hask_api.get_git_tree(head)
+print(gtree.tree)
 
 
 
